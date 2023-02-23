2ac3f62c49b6848523e6cbce7eae666250c5b5aa
Khoa Nguyen <khoapk01148@gmail.com>
2023-02-23 19:45:49 +0700

    - update source

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     trailingSlash: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/clinic' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/' : '',
     publicRuntimeConfig: {
         contextPath: process.env.NODE_ENV === 'production' ? '/clinic' : '',
         uploadPath: process.env.NODE_ENV === 'production' ? '/clinic/upload.php' : '/api/upload'
